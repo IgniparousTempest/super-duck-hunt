9437b24c218a4f5582aef156eb28474c7fe53d84
Courtney Pitcher <cr.pitcher@gmail.com>
2018-03-20 17:00:59 +0200

    Create animation.hpp

---
+++ animation.hpp
@@ -0,0 +1,27 @@
+#ifndef DUCKHUNT_ANIMATION_HPP
+#define DUCKHUNT_ANIMATION_HPP
+
+class Animation {
+private:
+    int currentFrame;
+    Timer timer;
+    std::vector<SDL_Rect> frames;
+public:
+    Animation(SDL_Texture* texture, int numberOfFrames, int framesPerSecond) : timer(1000.0 / framesPerSecond) {
+        auto framesArray = spriteStripRects<numberOfFrames>(texture);
+        frames.reserve(numberOfFrames);
+        std::copy_n(std::make_move_iterator(frames.begin()), numberOfFrames, framesArray.begin());
+        currentFrame = 0;
+    }
+
+    /// Advances the animation and returns the current frame.
+    /// \param deltaTime the time since the last frame.
+    /// \return the rect for the current frame.
+    const SDL_Rect* advance(double deltaTime) {
+        if (timer.tick(deltaTime))
+            currentFrame = ++currentFrame % frames.size();
+        return &frames[currentFrame];
+    }
+};
+
+#endif //DUCKHUNT_ANIMATION_HPP
