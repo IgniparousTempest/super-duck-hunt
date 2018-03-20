c7fd37cff84fd297f3d011ae9a6ee8e6b4d4d5d3
IgniparousTempest <cr.pitcher@gmail.com>
2018-03-21 01:58:00 +0200

    seperated sprite strip

--- animation.hpp
+++ animation.hpp
@@ -1,13 +1,19 @@
 #ifndef DUCKHUNT_ANIMATION_HPP
 #define DUCKHUNT_ANIMATION_HPP
 
+#include <algorithm>
+#include <vector>
+#include "SDL2/SDL.h"
+#include "timer.hpp"
+#include "textures.hpp"
+
 class Animation {
 private:
     int currentFrame;
     Timer timer;
     std::vector<SDL_Rect> frames;
 public:
-    Animation(SDL_Texture* texture, int numberOfFrames, int framesPerSecond) : timer(1000.0 / framesPerSecond) {
+    Animation(SDL_Texture* texture, unsigned int numberOfFrames, int framesPerSecond) : timer(1000.0 / framesPerSecond) {
         auto framesArray = spriteStripRects<numberOfFrames>(texture);
         frames.reserve(numberOfFrames);
         std::copy_n(std::make_move_iterator(frames.begin()), numberOfFrames, framesArray.begin());
